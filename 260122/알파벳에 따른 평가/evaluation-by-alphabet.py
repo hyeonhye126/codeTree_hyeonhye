e9b50edb7c0f27bcf253a45301c06ed0c2017a75
Hyeonhye <126955136+hyeonhye126@users.noreply.github.com>
2026-01-22 23:35:16 +0900

    Title: 알파벳에 따른 평가, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 260122/알파벳에 따른 평가/evaluation-by-alphabet.py
@@ -0,0 +1,17 @@
+c = input()
+string = ''
+
+if c == 'S':
+    string = "Superior"
+elif c == 'A':
+    string = "Excellent"
+elif c == 'B':
+    string = "Good"
+elif c == 'C':
+    string = "Usually"
+elif c == 'D':
+    string = "Effort"
+else:
+    string = "Failure"
+
+print(string)
